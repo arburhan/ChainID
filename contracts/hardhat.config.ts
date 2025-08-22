c6d5acdfb7da00108c293e5193191b570dbef0a0
arburhan <burhan128454@outlook.com>
2025-08-22 20:23:12 +0600

    setup smart contracts env

---
+++ contracts/hardhat.config.ts
@@ -0,0 +1,26 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({ path: path.resolve(process.cwd(), "../.env") });
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.24",
+    settings: {
+      optimizer: { enabled: true, runs: 200 }
+    }
+  },
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL || "",
+      accounts: process.env.SEPOLIA_PRIVATE_KEY ? [process.env.SEPOLIA_PRIVATE_KEY] : [],
+    }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY || ""
+  }
+};
+
+export default config;
